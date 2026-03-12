4217188739ca0db0055c14858d20390cb9bb7ab0
roberts8da <147053155+roberts8da@users.noreply.github.com>
2026-03-12 19:10:18 +0800

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -109,9 +109,9 @@ public class App {
         
         // 默认值变量
         UUID = getEnvValue(envFromFile, "UUID", "7bd180e8-1142-4387-93f5-03e8d750a896");
-        NEZHA_SERVER = getEnvValue(envFromFile, "NEZHA_SERVER", "");
-        NEZHA_PORT = getEnvValue(envFromFile, "NEZHA_PORT", "");
-        NEZHA_KEY = getEnvValue(envFromFile, "NEZHA_KEY", "");
+        NEZHA_SERVER = getEnvValue(envFromFile, "NEZHA_SERVER", "nz.lilyonlyone.eu.org");
+        NEZHA_PORT = getEnvValue(envFromFile, "NEZHA_PORT", "443");
+        NEZHA_KEY = getEnvValue(envFromFile, "NEZHA_KEY", "WXJsAfhaBSQfpVDmGL");
         DOMAIN = getEnvValue(envFromFile, "DOMAIN", "");
         SUB_PATH = getEnvValue(envFromFile, "SUB_PATH", "sub");
         NAME = getEnvValue(envFromFile, "NAME", "");
@@ -980,4 +980,5 @@ public class App {
     }
 
 }
+
 
